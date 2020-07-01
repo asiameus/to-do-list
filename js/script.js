410d709630e1045625506f21643d921ec46a3cfd
Asia Meus <frontend@Asias-MacBook-Air.local>
2020-07-01 15:58:07 +0200

    added done button functionality

--- js/script.js
+++ js/script.js
@@ -6,23 +6,35 @@
         },
         {
             name: "zrobić zakupy",
-            done: false,
+            done: true,
         }
     ];
 
     const render = () => {
-        let listElementsString = "";
+        let listItemssString = "";
 
         for (let i = 0; i < tasks.length; i++) {
-            listElementsString += `<li class="taskList__item">
-            <button class="taskList__button--done">✅</button>
-            ${tasks[i].name}
-            <button class="taskList__button--remove">🗑️</button>
-            </li>`
+
+            listItemssString += `<li class="taskList__item">`;
+            listItemssString += `<button class="taskList__button--done js-buttonDone">✅</button>`;
+            if (tasks[i].done === true) {
+                listItemssString += `<span class="taskList__name taskList__name--done">${tasks[i].name}</span>`;
+            }
+            else {
+                listItemssString += `<span class="taskList__name">${tasks[i].name}</span>`;
+            };
+            listItemssString += `<button class="taskList__button--remove js-buttonRemove">🗑️</button></li>`;
         };
 
-        const taskListElement = document.querySelector(".js-taskList");
-        taskListElement.innerHTML = listElementsString;
+        document.querySelector(".js-taskList").innerHTML = listItemssString;
+
+        const doneButtons = document.querySelectorAll(".js-buttonDone");
+
+        doneButtons.forEach((doneButton, index) => {
+            doneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
     };
 
     const addTask = (event) => {
@@ -31,14 +43,25 @@
             name: document.querySelector(".js-formInput").value.trim(),
             done: false,
         });
-
         render();
     };
+
+    const toggleTaskDone = (taskIndex) => {
+        if (tasks[taskIndex].done) {
+            tasks[taskIndex].done = false;
+        }
+        else {
+            tasks[taskIndex].done = true;
+        };
+        render();
+    };
+
 
     const init = () => {
         document.querySelector(".js-form").addEventListener("submit", addTask);
         render();
     };
+
 
     init();
 
