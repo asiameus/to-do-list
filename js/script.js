665e90db70d92d03869aba23081504b4cc3f962b
Asia Meus <frontend@Asias-MacBook-Air.local>
2020-07-03 18:19:11 +0200

    onFormSubmit and render functions refactoring

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@
 
             listItemssString += `<li class="taskList__item">`;
             if (task.done === true) {
-                listItemssString += `<button class="taskList__button taskList__button--toggleDone js-buttonDone"><img src="img/check.svg"></button>`;
-                listItemssString += `<span class="taskList__name taskList__name--done">${task.name}</span>`;
+                listItemssString +=
+                    `<button class="taskList__button taskList__button--toggleDone js-buttonDone"><img src="img/check.svg"></button>
+                    <span class="taskList__name taskList__name--done">${task.name}</span>`;
             }
             else {
-                listItemssString += `<button class="taskList__button taskList__button--toggleDone js-buttonDone"></button>`;
-                listItemssString += `<span class="taskList__name">${task.name}</span>`;
+                listItemssString +=
+                    `<button class="taskList__button taskList__button--toggleDone js-buttonDone"></button>
+                    <span class="taskList__name">${task.name}</span>`;
             };
             listItemssString += `<button class="taskList__button taskList__button--remove js-buttonRemove"><img src="img/trash-2.svg"></button></li>`;
         };
@@ -59,23 +61,17 @@
         render();
     }
 
-    const resetInput = (formInputElement) => {
-        formInputElement.value = "";
-        formInputElement.focus();
-    }
-
     const onFormSubmit = (event) => {
         event.preventDefault();
         const formInputElement = document.querySelector(".js-formInput");
         const newTaskContent = formInputElement.value.trim();
 
-        if (newTaskContent === "") {
-            formInputElement.focus();
-            return;
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            formInputElement.value = "";
         }
 
-        addNewTask(newTaskContent);
-        resetInput(formInputElement);
+        formInputElement.focus();
     };
 
     const init = () => {
